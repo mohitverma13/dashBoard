9d391d6a215a6bc39e152753b6d0d754759fc179
Mohit Kumar <mohitverma8822@gmail.com>
2024-10-13 02:48:41 +0530

    BlogCard fixed

--- src/components/BlogPostCard.jsx
+++ src/components/BlogPostCard.jsx
@@ -14,7 +14,11 @@ export default function BlogPostCard() {
     return (
         <Card className='flex flex-col w-[20rem]'>
             <CardHeader className='flex flex-row justify-start items-center'>
-                <h3 className="text-lg font-semibold text-left">How to sell courses blog</h3>
+                <h4 className=" text-left flex justify-center items-center text-base gap-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+                    </svg>
+                    How to sell courses blog</h4>
                 <button className='bg-[#3bd7c5] rounded-xl text-sm w-[50px] flex items-center justify-center mx-2'> see all</button>
             </CardHeader>
             <CardContent>
@@ -22,7 +26,7 @@ export default function BlogPostCard() {
                     {blogPosts.map((post, index) => (
                         <li key={index} className="flex flex-col sm:flex-row sm:justify-between">
                             <a href="#" className="text-sm text-teal-500 hover:underline">{post.title}</a>
-                            <span className="text-sm text-gray-500 mt-1 sm:mt-0">{post.daysAgo} days ago</span>
+                            <span className="text-xs text-gray-500 mt-1 sm:mt-0">{post.daysAgo} days ago</span>
                         </li>
                     ))}
                 </ul>
